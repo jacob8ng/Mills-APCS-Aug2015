4ec487730a53870b520fc25f8aa0cdd5f40bf5af
jacob8ng <jacob8ng@yahoo.com>
2015-08-25 09:28:53 -0700

    Mississippi
    UGH

---
+++ Mississippi/Mississippi.java
@@ -0,0 +1,47 @@
+public class Mississippi{
+    public static void main(String[] args){
+        m();
+        i();
+        s();
+        s();
+        i();
+        s();
+        s();
+        i();
+        p();
+        p();
+        i();
+    }
+    public static void m(){
+        twoM();
+        System.out.println("MM   MM");
+        System.out.println("M M M M");
+        System.out.println("M  M  M");
+        twoM();
+        twoM();
+        twoM();
+    }
+    public static void twoM(){
+        System.out.println("M     M");
+    }
+    public static void i(){
+        fiveI();
+        System.out.println("   I");
+        System.out.println("   I");
+        System.out.println("   I");
+        System.out.println("   I");
+        System.out.println("   I");
+        fiveI();
+    }
+    public static void fiveI(){
+        System.out.println(" IIIII");
+    }
+    public static void s(){
+        fiveS();
+        twoS();
+        System.out.println("S");
+        fiveS();
+        System.out.println("      S");
+        twoS();
+        fiveS();
+    }
